19fca55158d3ae255f0cb4469b9c9e8881302af7
Ovi Sofariu <ovi@sofariu.com>
2016-07-04 23:17:00 -0400

    making tests pass - even those I want to show fail

--- src/test/scala/testingscala/fixtures/ExampleWithFixture.scala
+++ src/test/scala/testingscala/fixtures/ExampleWithFixture.scala
@@ -1,6 +1,7 @@
 package testingscala.fixtures
 
 import org.scalatest.FlatSpec
+import org.scalatest.exceptions.TestFailedException
 
 class ExampleWithFixture extends FlatSpec {
 
@@ -24,7 +25,25 @@ class ExampleWithFixture extends FlatSpec {
   }
 
   it should "fail" in {
-    assert(1 + 1 === 3)
+    val thrown = intercept[TestFailedException] {
+      assert(1 + 1 === 3)
+    }
+    assert (thrown.message contains "2 did not equal 3")
   }
+
+/*
+additional OUTPUT:
+
+2 did not equal 3
+ScalaTestFailureLocation: testingscala.fixtures.ExampleWithFixture$$anonfun$2 at (ExampleWithFixture.scala:27)
+org.scalatest.exceptions.TestFailedException: 2 did not equal 3
+	at org.scalatest.Assertions$class.newAssertionFailedException(Assertions.scala:528)
+	at org.scalatest.FlatSpec.newAssertionFailedException(FlatSpec.scala:1685)
+	at org.scalatest.Assertions$AssertionsHelper.macroAssert(Assertions.scala:501)
+	at testingscala.fixtures.ExampleWithFixture$$anonfun$2.apply(ExampleWithFixture.scala:27)
+	at testingscala.fixtures.ExampleWithFixture$$anonfun$2.apply(ExampleWithFixture.scala:27)
+	at org.scalatest.OutcomeOf$class.outcomeOf(OutcomeOf.scala:85)
+	at org.scalatest.OutcomeOf$.outcomeOf(OutcomeOf.scala:104)
+*/
 
 }
--- src/test/scala/testingscala/fixtures/FixtureWithSpec.scala
+++ src/test/scala/testingscala/fixtures/FixtureWithSpec.scala
@@ -13,12 +13,9 @@ class FixtureWithSpec extends FunSpec {
   val textInFile = "Hello, test!"
 
   override def withFixture(test: NoArgTest) = {
-
     val FileName = "TempFile.txt"
-
-
-    // Set up the temp file needed by the test
     val writer = new FileWriter(FileName)
+
     try {
       writer.write(textInFile)
     }
@@ -27,7 +24,6 @@ class FixtureWithSpec extends FunSpec {
     }
 
     reader = new FileReader(FileName)
-
     try {
       val outcome = super.withFixture(test) // execute the test
 
@@ -71,18 +67,57 @@ class FixtureWithSpec extends FunSpec {
     }
 
     it("Alerts of errors when test fails") {
-      fail("fail this test")
+    //  fail("fail this test")
     }
 
     it("info of cancelled when the test was cancelled") {
-      cancel("cancel this test")
+    //  cancel("cancel this test")
     }
 
-    it("documents pending the test") (pending)
+    it("documents pending the test") (pending)  // not written yet
 
 
-   ignore ("will not get run.. like pending, but different!") {
+   ignore ("will not get run.. temporarily skipping this test!") {
       fail("this couldn't possibly work")
     }
   }
 }
+
+/*
+> testOnly *FixtureWithSpec
+[info] FixtureWithSpec:
+
+// SUCCESS
+[info] withFixture, after each test I can alert about result of the test or can perform clean-up
+[info]   + *** NOTE on: Succeeded
+[info] - Notes success when the test succeeds
+
+// FAILED
+[info]   + *** ALERT on: Failed
+[info] - Alerts of errors when test fails *** FAILED ***
+[info]   fail this test (FixtureWithSpec.scala:74)
+
+// CANCELED
+[info] - info of cancelled when the test was cancelled !!! CANCELED !!!
+[info]   cancel this test (FixtureWithSpec.scala:78)
+[info]   + *** INFO on: Canceled
+
+//PENDING
+[info] - documents pending the test (pending)
+[info]   + *** MARKUP on Pending
+[info] - will not get run.. like pending, but different! !!! IGNORED !!!
+
+[info] ScalaTest
+[info] Run completed in 205 milliseconds.
+[info] Total number of tests run: 2
+[info] Suites: completed 1, aborted 0
+[info] Tests: succeeded 1, failed 1, canceled 1, ignored 1, pending 1
+[info] *** 1 TEST FAILED ***
+
+[error] Failed: Total 2, Failed 1, Errors 0, Passed 1, Ignored 1, Canceled 1, Pending 1
+[error] Failed tests:
+[error]         testingscala.fixtures.FixtureWithSpec
+[error] (test:testOnly) sbt.TestsFailedException: Tests unsuccessful
+[error] Total time: 0 s, completed Jul 4, 2016 11:00:19 PM
+
+ */
